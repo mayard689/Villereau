54a5550fd8d3da4677c0b8628afdfcb9c7c4ccfd
mayard689 <doc-albert@laposte.net>
2020-10-29 16:29:55 +0100

    navbar done

--- assets/app.js
+++ assets/app.js
@@ -14,4 +14,6 @@ const $ = require('jquery');
 
 require('bootstrap');
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
