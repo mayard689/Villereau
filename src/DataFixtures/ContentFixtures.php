76be6d1706c8c44512a5af0d616f92dc71450da0
mayard689 <doc-albert@laposte.net>
2020-10-30 11:02:28 +0100

    content fixture update to change themanually written content

--- src/DataFixtures/ContentFixtures.php
+++ src/DataFixtures/ContentFixtures.php
@@ -13,25 +13,8 @@ class ContentFixtures extends Fixture
 
     const CONTENT = [
       0 => [
-          'title' => 'Gagner de l\'argent, Sauver la planète, prendre un bain.',
-          'text' => 'Vous êtes dans votre baignoire, musique relaxante, mousse senteur des îles, eau tiède ou chaude, 
-                  comme vous aimez. Oublié les ennuis du boulot, et le dérèglement climatique et les enfants qui courent partout, 
-                  c’est le temps d’un instant pour soi, allégé par la poussé d’Archimède bref, relaxation, décontraction, 
-                  déconnexion.
-
-                  Mais c’est là qu’arrive le “Un bain ! Ça consomme trop ! Trop d’électricité pour chauffer toute cette eau perdue. La 
-                  douche c’est plus économique”. Mais tout dépend de la durée de la douche, vous ne croyez pas ?
-                    
-                  Auriez vous imaginé qu’au delà de 7 minutes, une douche consomme plus d’eau et de chauffage qu’un bain de 30 minutes. Chaque minute sous l’eau 
-                  chaude coûte environ 27 euros par an, 54€ si vous êtes deux...Vous pourriez tout autant vous offrir un dîner aux 
-                  chandelles et trouver un endroit au calme pour admirer les étoiles.
-                    
-                  On est bien d’accord : personne n’a envie de se retrouver dans un dîner en tête à tête avec les cheveux gras à cause 
-                  d’une toilette expédiée. 
-                    
-                  L’idée c’est juste que si, comme beaucoup de français vous passez 9 minutes sous la douche, vous pouvez sans doute 
-                  diminuer à 8 minutes et prendre un bon bain bien long de temps en temps. Prenez soin de vous et prenez du temps pour 
-                  vous.',
+          'title' => 'Ceci est un exemple de titre.',
+          'text' => 'Ceci est un exemple de texte.',
           'picture' => 'douche.jpeg',
       ],
     ];
