f38515509106b0d52053a07d8408c4aef5616da9
mayard689 <doc-albert@laposte.net>
2020-11-08 09:35:10 +0100

    authentication done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(EventRepository $eventRepository, ContentRepository $contentRepository): Response
     {
