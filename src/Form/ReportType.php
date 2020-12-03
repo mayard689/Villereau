356f9efa3aed0cc1021969193ef520af5e2f75d1
mayard689 <doc-albert@laposte.net>
2020-12-03 17:05:16 +0100

    upload fixed

--- src/Form/ReportType.php
+++ src/Form/ReportType.php
@@ -6,15 +6,23 @@ use App\Entity\Report;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ReportType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('document')
             ->add('date', null, [
                 'format' => 'dd-MMM-yyyy',
+            ])
+            ->add('documentFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => false,
+                'download_label' => '...',
+                'download_uri' => false,
+                'image_uri' => true,
+                'asset_helper' => true,
             ])
         ;
     }
